472f705a1f87900b309ba81ece879121335af287
Sanyam Kapoor <sanyam@nyu.edu>
2023-08-23 07:00:09 -0400

    resume any scheduler from any step

--- llm/utils/scheduler.py
+++ llm/utils/scheduler.py
@@ -4,19 +4,20 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 
 
 class AnyCosineScheduler:
-    """Repurpose torch schedulers for all scalars (any non-nn.Parameter)."""
+    """Hacky repurpose of torch schedulers for all scalars (any non-nn.Parameter)."""
 
     def __init__(self, init_value, T_max, last_epoch=-1, **kwargs) -> None:
         p = nn.Parameter(torch.randn(1))
         self._T_max = T_max
         self._opt = torch.optim.SGD([p], lr=init_value)
-        self._sched = CosineAnnealingLR(
-            self._opt, T_max, last_epoch=last_epoch, **kwargs
-        )
+        self._sched = CosineAnnealingLR(self._opt, T_max, **kwargs)
+        if last_epoch > 0:
+            for _ in range(last_epoch):
+                self.step()
 
     @property
     def value(self):
-        return self._sched.get_lr()[0]
+        return self._sched.get_last_lr()[0]
 
     def step(self):
         if self._T_max > 0:
