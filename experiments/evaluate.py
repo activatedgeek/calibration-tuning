b3a2394844812e9311ff2c2d5ca210bf9b1018a9
Sanyam Kapoor <sanyam@nyu.edu>
2023-09-09 13:48:20 -0400

    merge evaluate scripts

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -1,4 +1,5 @@
 import logging
+from tqdm.auto import tqdm
 import wandb
 import pandas as pd
 import torch
@@ -6,6 +7,7 @@ from accelerate import Accelerator
 from peft import PeftModel
 
 from llm.logging import entrypoint
+from llm.datasets import list_datasets, get_dataset_attrs
 from llm.models import get_model
 from llm.utils.evaluation import evaluate_dataset_via_eos
 from llm.utils.trainer import get_last_checkpoint_path
@@ -27,7 +29,6 @@ def main(
 
     config = {
         "seed": seed,
-        "dataset": dataset,
         "model_name": model_name,
         "model_dir": model_dir,
         "peft_dir": peft_dir,
@@ -57,28 +58,47 @@ def main(
 
         logging.info(f"Loaded PEFT checkpoint from '{peft_dir}'")
 
-    val_metrics, test_metrics = evaluate_dataset_via_eos(
-        accelerator,
-        model,
-        tokenizer,
-        dataset,
-        seed=seed,
-        batch_size=batch_size,
-        data_dir=data_dir,
-        eval_kshot=eval_kshot,
-        use_cache=use_dataset_cache,
-    )
-
-    all_metrics = list(
-        map(
-            lambda m: {**m, **config, "dataset": dataset},
-            list(filter(lambda m: m is not None, [val_metrics, test_metrics])),
+    if dataset is None:
+        all_datasets = sorted(
+            list(
+                filter(
+                    lambda x: ("combined" not in x)
+                    and ("mmlu" not in x)
+                    and ("bbh" not in x),
+                    list_datasets(),
+                )
+            )
+            + [f"mmlu:{task}" for task in get_dataset_attrs("mmlu").get("tasks")]
         )
-    )
-    logging.info(
-        {"metrics": wandb.Table(dataframe=pd.DataFrame(all_metrics))},
-        extra=dict(metrics=True),
-    )
+        logging.warning("No dataset argument used. Evaluating all datasets.")
+    else:
+        all_datasets = [dataset]
+
+    all_metrics = []
+    for dataset in tqdm(all_datasets):
+        val_metrics, test_metrics = evaluate_dataset_via_eos(
+            accelerator,
+            model,
+            tokenizer,
+            dataset,
+            seed=seed,
+            batch_size=batch_size,
+            data_dir=data_dir,
+            eval_kshot=eval_kshot,
+            use_cache=use_dataset_cache,
+        )
+
+        dataset_metrics = list(
+            map(
+                lambda m: {**m, **config, "dataset": dataset},
+                list(filter(lambda m: m is not None, [val_metrics, test_metrics])),
+            )
+        )
+        all_metrics += dataset_metrics
+        logging.info(
+            {"metrics": wandb.Table(dataframe=pd.DataFrame(all_metrics))},
+            extra=dict(metrics=True),
+        )
 
 
 if __name__ == "__main__":
