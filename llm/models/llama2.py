fix temperature scaling for uncertainty tuning

llm/models/llama2.py
@@ -7,13 +7,14 @@ import torch.nn as nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from peft import PeftModel
 
-from transformers import LlamaTokenizer, LlamaForCausalLM, LlamaPreTrainedModel
+from transformers import LlamaTokenizer, LlamaPreTrainedModel
 from transformers.modeling_outputs import SequenceClassifierOutputWithPast
 from transformers.utils import WEIGHTS_NAME
 from peft import prepare_model_for_kbit_training
 
 from .registry import register_model
 from .llm_utils import get_special_tokens
+from .peft.temperature_scaling import TemperatureScaledLlamaForCausalLM
 
 
 __all__ = ["create_tokenizer", "create_model"]
@@ -194,7 +195,7 @@ def create_model(
     **kwargs,
 ):
     if causal_lm:
-        model = LlamaForCausalLM.from_pretrained(
+        model = TemperatureScaledLlamaForCausalLM.from_pretrained(
             model_dir or f"meta-llama/Llama-2-{size}-hf",
             cache_dir=os.environ.get("MODELDIR", cache_dir),
             load_in_8bit=load_in_8bit,

llm/models/peft/temperature_scaling.py
@@ -1,102 +1,55 @@
 import logging
-import os
 import torch
 import torch.nn as nn
-from accelerate import PartialState as AcceleratorState
-from transformers import TrainerCallback
-
-from .utils import get_last_checkpoint_path
-from ..utils import setchainattr, getchainattr
+import torch.nn.functional as F
+from transformers import LlamaForCausalLM
 
 
 WEIGHTS_NAME = "temperature_adapter.bin"
 
 
-class TemperatureScale(nn.Module):
-    PARAMETER_NAME = "log_temperature"
+class TemperatureScaledLlamaForCausalLM(LlamaForCausalLM):
+    PARAMETER_NAME = "temperature"
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.log_temperature = nn.Parameter(torch.zeros(1))
+        self.register_parameter(
+            self.PARAMETER_NAME, nn.Parameter(torch.zeros(1), requires_grad=False)
+        )
 
-    def forward(self, logits):
-        t = self.log_temperature.exp()
-        return logits / t
+    def forward(self, *args, scale_temp=False, **kwargs):
+        outputs = super().forward(*args, **kwargs)
 
+        if scale_temp:
+            T = F.softplus(getattr(self, self.PARAMETER_NAME))
+            outputs.logits = outputs.logits / T
 
-def get_temperature_scaled_model(
-    model, module_name="lm_head", checkpoint_dir=None, is_trainable=True
-):
-    assert hasattr(model, module_name), f"{module_name} not found in model."
-
-    accelerator = AcceleratorState()
-
-    temperature_module = TemperatureScale()
-    if checkpoint_dir is not None:
-        checkpoint_dir = get_last_checkpoint_path(checkpoint_dir)
-
-        if os.path.isfile(f"{checkpoint_dir}/{WEIGHTS_NAME}"):
-            ## Remove prefixes while loading to avoid name conflicts.
-            state_dict = {
-                k.split(".")[-1]: v
-                for k, v in torch.load(f"{checkpoint_dir}/{WEIGHTS_NAME}").items()
-            }
-            temperature_module.load_state_dict(state_dict)
-
-            logging.info(f"Loaded temperature adapter from '{checkpoint_dir}'")
-
-    target_module_names = [
-        n for n, _ in model.named_modules() if module_name == n.split(".")[-1]
-    ]
-    assert (
-        len(target_module_names) == 1
-    ), f'Ambiguous module name "{module_name}" provided. Found {target_module_names}'
-
-    module_name = target_module_names[0]
-
-    if is_trainable:
-        logging.debug(f"Freezing existing model parameters.")
-        for _, p in model.named_parameters():
-            p.requires_grad_(False)
-    else:
-        for _, p in temperature_module.named_parameters():
-            p.requires_grad_(False)
-
-    ## Hotpatch temperature module.
-    setchainattr(
-        model,
-        module_name,
-        nn.Sequential(
-            getchainattr(model, module_name), temperature_module.to(accelerator.device)
-        ),
-    )
-
-    return model
+        return outputs
 
 
-def save_temperature_scaled_model(model, path):
+def prepare_model_for_temperature_scaling(model):
+    for n, p in model.named_parameters():
+        if TemperatureScaledLlamaForCausalLM.PARAMETER_NAME in n:
+            p.requires_grad_(True)
+        else:
+            if p.requires_grad:
+                p.requires_grad_(False)
+
+
+def save_temperature_scaled_model(model, output_dir):
     ## Assumes scaling used only once, strip of module path for independent reloading.
     state_dict = {
-        k.split(".")[-1]: v
+        k: v
         for k, v in model.state_dict().items()
-        if TemperatureScale.PARAMETER_NAME in k
+        if TemperatureScaledLlamaForCausalLM.PARAMETER_NAME in k
     }
 
     if not len(state_dict):
         logging.warning(
-            f"Parameter {TemperatureScale.PARAMETER_NAME} not found. Skipping save."
+            f"Parameter {TemperatureScaledLlamaForCausalLM.PARAMETER_NAME} not found. Skipping save."
         )
         return
 
-    os.makedirs(path, exist_ok=True)
-    with open(f"{path}/{WEIGHTS_NAME}", "wb") as f:
+    with open(f"{output_dir}/{WEIGHTS_NAME}", "wb") as f:
         torch.save(state_dict, f)
-
-
-class TemperatureSaveCallback(TrainerCallback):
-    def on_save(self, args, state, *_, model=None, **_kwargs):
-        if state.is_world_process_zero:
-            checkpoint_path = f"{args.output_dir}/checkpoint-{state.global_step}"
-
-            save_temperature_scaled_model(model, checkpoint_path)

llm/utils/uncertainty_tuner.py
@@ -1,10 +1,11 @@
+import os
 from dataclasses import dataclass, field
 import torch
 import torch.nn.functional as F
 from torch.distributions import Categorical, kl_divergence
 from transformers import Trainer
+from transformers.trainer import logger, TRAINING_ARGS_NAME
 from transformers.training_args import TrainingArguments
-from peft import PeftModel
 
 from ..datasets.llm_utils import (
     DataCollatorForSupervisedDataset,
@@ -13,14 +14,16 @@ from ..datasets.llm_utils import (
     IGNORE_LABEL,
 )
 from ..eval import evaluate_dataset
+from ..models.peft import save_temperature_scaled_model
 
 
 class UncertaintyTuner(Trainer):
     @dataclass
     class Args(TrainingArguments):
         query_format: str = field(default="roman_choice")
-        unc_normalize: bool = field(default=True)
         unc_label_smoothing: float = field(default=0.0)
+        kl_decay: float = field(default=1.0)
+        scale_temp: bool = field(default=False)
         ref_adapter_name: str = field(default="_ref")
         kl_decay: float = field(default=1.0)
 
@@ -32,11 +35,6 @@ class UncertaintyTuner(Trainer):
             data_collator=DataCollatorForSupervisedDataset(tokenizer),
         )
 
-        assert isinstance(self.model, PeftModel), f"Model should be a PeftModel"
-        assert set(self.model.peft_config.keys()) == set(
-            ["default", self.args.ref_adapter_name]
-        ), f"PeftModel should have 'default' and '{self.args.ref_adapter_name}' adapters"
-
         self.val_data = val_data
         self.test_data = test_data
 
@@ -46,26 +44,21 @@ class UncertaintyTuner(Trainer):
         )
         query_inputs = self.data_collator(query_inputs)
 
-        if self.args.unc_normalize:
-            query_outputs = model(**query_inputs)
+        query_outputs = model(**query_inputs, scale_temp=self.args.scale_temp)
 
-            _, unc_y, unc_logits = extract_qa_exact(
-                self.tokenizer, query_inputs, outputs=query_outputs
-            )
-            unc_y, unc_logits = (
-                (unc_y.unsqueeze(-1) == query_token_vec).long().argmax(dim=-1),
-                unc_logits[:, query_token_vec],
-            )
+        _, unc_y, unc_logits = extract_qa_exact(
+            self.tokenizer, query_inputs, outputs=query_outputs
+        )
+        unc_y, unc_logits = (
+            (unc_y.unsqueeze(-1) == query_token_vec).long().argmax(dim=-1),
+            unc_logits[:, query_token_vec],
+        )
 
-            unc_loss = F.cross_entropy(
-                unc_logits,
-                unc_y.to(unc_logits.device),
-                label_smoothing=self.args.unc_label_smoothing,
-            )
-
-            return unc_loss
-
-        unc_loss = super().compute_loss(model, query_inputs)
+        unc_loss = F.cross_entropy(
+            unc_logits,
+            unc_y.to(unc_logits.device),
+            label_smoothing=self.args.unc_label_smoothing,
+        )
 
         return unc_loss
 
@@ -86,10 +79,10 @@ class UncertaintyTuner(Trainer):
         # p_mix = Categorical(probs=mix_probs)
         # raw_loss = (kl_divergence(p, p_mix) + kl_divergence(p_ref, p_mix)) / 2
 
-        raw_loss = kl_divergence(p, p_ref)
+        forward_kl = kl_divergence(p, p_ref)
 
         loss_mask = labels != IGNORE_LABEL
-        loss = (raw_loss * loss_mask).sum(dim=-1).mean(dim=0)
+        loss = (forward_kl * loss_mask).sum(dim=-1).mean(dim=0)
 
         return loss
 
@@ -97,7 +90,10 @@ class UncertaintyTuner(Trainer):
         loss, outputs = super().compute_loss(model, inputs, return_outputs=True)
 
         unc_loss = self.compute_unc_loss(model, inputs, outputs)
-        kl_loss = self.compute_kl_loss(model, inputs, outputs)
+        if self.args.scale_temp:
+            kl_loss = torch.tensor(0.0)
+        else:
+            kl_loss = self.compute_kl_loss(model, inputs, outputs)
 
         total_loss = unc_loss + self.args.kl_decay * kl_loss
 
@@ -140,3 +136,23 @@ class UncertaintyTuner(Trainer):
             metrics.update(test_metrics)
 
         return metrics
+
+    def _save(self, output_dir=None, state_dict=None):
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving model checkpoint to {output_dir}")
+
+        self.model.save_pretrained(
+            output_dir,
+            state_dict=state_dict,
+            safe_serialization=self.args.save_safetensors,
+            selected_adapters=["default"],
+        )
+
+        if self.args.scale_temp:
+            save_temperature_scaled_model(self.model, output_dir)
+
+        if self.tokenizer is not None:
+            self.tokenizer.save_pretrained(output_dir)
+
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))