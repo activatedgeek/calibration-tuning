bee8c3a46b1ef7d244452f6f4285ea6a5c8e7959
Sanyam Kapoor <sanyam@nyu.edu>
2024-01-23 20:06:56 -0500

    fix offline dataset column names

--- llm/datasets/offline.py
+++ llm/datasets/offline.py
@@ -12,20 +12,23 @@ def get_offline(
 ):
     from datasets import load_dataset, Features, Value
 
-    features = Features({
-        'context': Value('string'),
-        'target': Value('string'),
-        'target_prompt': Value('string'),
-        'prompt': Value('string'),
-        'source_dataset': Value('string'),
-        'output': Value('string'),
-    })
+    features = Features(
+        {
+            "context": Value("string"),
+            "target": Value("string"),
+            "target_prompt": Value("string"),
+            "prompt": Value("string"),
+            "source_dataset": Value("string"),
+            "output": Value("string"),
+            "query_label": Value("int32"),
+        }
+    )
 
     dataset = load_dataset(
         "csv",
         data_files=dict(train=glob.glob(f"{root}/train/*.csv")),
         cache_dir=os.environ.get("HF_DATASETS_CACHE", root),
-        features=features
+        features=features,
     )
     if not use_cache:
         dataset.cleanup_cache_files()
